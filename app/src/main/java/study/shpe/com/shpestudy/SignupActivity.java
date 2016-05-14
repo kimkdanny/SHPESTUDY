bbfe268c90a4f468b91573ebab822a985e91e2cf
Eduardo Marin <marin3e20@gmail.com>
2016-05-14 16:11:37 -0700

    eduardos commit

---
+++ app/src/main/java/study/shpe/com/shpestudy/SignupActivity.java
@@ -0,0 +1,58 @@
+package study.shpe.com.shpestudy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+
+import java.lang.reflect.Array;
+
+public class SignupActivity extends AppCompatActivity  {
+
+    EditText userEmail, userName;
+    Button userLogin;
+    Intent j;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("SIGN UP");
+        Firebase myFirebaseRef = new Firebase("https://shpestudy.firebaseio.com/");
+        final Firebase userFirebase = myFirebaseRef.child("users");
+
+        setContentView(R.layout.signup);
+        userEmail= (EditText) findViewById(R.id.myEmail);
+        userName= (EditText) findViewById(R.id.myName);
+        userLogin= (Button) findViewById(R.id.submit);
+        j = new Intent(SignupActivity.this,SampleActivity.class);
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = userEmail.getText().toString();
+                boolean valid = email.contains("@")&& email.contains(".");
+                if(valid) {
+                    Firebase firebaseObj = userFirebase.push();
+                        firebaseObj.child("name").setValue(userName.getText().toString());
+                    firebaseObj.child("email").setValue(userEmail.getText().toString());
+                    Toast.makeText(getApplicationContext(),"You have signed up!",Toast.LENGTH_SHORT).show();
+                    userEmail.setText("");
+                    userName.setText("");
+                    startActivity(j);
+                    finish();
+
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Email is not valid", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/study/shpe/com/shpestudy/User.java
@@ -0,0 +1,20 @@
+package study.shpe.com.shpestudy;
+
+/**
+ * Created by Eduardo on 5/14/2016.
+ */
+public class User {
+    String name;
+    String email;
+    public User(){
+
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
