76f192d081947b982cc08dc862a048a171768512
Danny <shpeucsd.codeteam@gmail.com>
2016-05-14 11:26:13 -0700

    Ricardo's Touch Draw

---
+++ app/src/main/java/study/shpe/com/shpestudy/TouchEventView.java
@@ -0,0 +1,63 @@
+package stuff.ricardo.com.ricardosapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.SizeF;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Kim on 5/7/16.
+ */
+public class TouchEventView extends View{
+    private Paint paint = new Paint();
+    private Path path = new Path();
+    private Canvas c = new Canvas();
+
+    public TouchEventView(Context ctx, AttributeSet attrs) {
+        super(ctx, attrs);
+        setDrawingCacheEnabled(true);
+
+        paint.setAntiAlias(true);
+        paint.setColor(Color.WHITE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5f);
+        this.setBackgroundColor(Color.BLACK);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        canvas.drawPath(path, paint);
+        c = canvas;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        float xpos = event.getX();
+        float ypos = event.getY();
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(xpos, ypos);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(xpos,ypos);
+                break;
+            case MotionEvent.ACTION_UP://do nothing finger lifted
+                break;
+            default:
+                return false;
+        }
+
+
+        //schedule a repaint
+        invalidate();
+        return true;
+    }
+
+}
---
+++ app/src/main/java/study/shpe/com/shpestudy/WriteOnScreenActivity.java
@@ -0,0 +1,18 @@
+package stuff.ricardo.com.ricardosapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class WriteOnScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_on_screen);
+        LinearLayout myLInearLayout = (LinearLayout) findViewById(R.id.canvas);
+        myLInearLayout.addView(new TouchEventView(this, null));
+    }
+
+
+}
